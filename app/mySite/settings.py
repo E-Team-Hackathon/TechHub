fc56eaab7a4c4ef09fab518a603358a8d6613e5e
megane575 <rie-fttm0129@gmail.com>
2025-01-27 21:19:02 +0900

    ＃1DBをmysqlに設定

--- app/mySite/settings.py
+++ app/mySite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techhub_db',
+        'USER': 'testuser',
+        'PASSWORD': 'testuser',
+        'HOST': 'mysql',
+        'PORT': 3306,
     }
 }
 
