22daf0f9242519c43107370a628905ab1c5fad90
megane575 <rie-fttm0129@gmail.com>
2025-01-30 11:33:23 +0900

    #1 Usersテーブルよりemailを削除

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -7,23 +7,31 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("ユーザー名を入力してください")
+        if not password:
+            raise ValueError("パスワードを入力してください")
+        
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        return self.create_user(username, email, password, **extra_fields)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        return self.create_user(username, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, max_length=255)
-    email = models.EmailField(unique=True)
-    description = models.CharField(max_length=255, blank=True, default="")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -31,5 +39,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    #ログインする時にはusernameを使用
+    USERNAME_FIELD = 'username'
+    # 管理コマンドを使用してスーパーユーザーを作成する際に必要とされるフィールド
+    REQUIRED_FIELDS = ['password']
+
+    # 管理者などで区別がつきやすいようにusernameで表示
+    def __str__(self):
+        return self.username
