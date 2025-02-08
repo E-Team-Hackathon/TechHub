acd63ace63383fb8346b9f3bf18c5fa8e8d74b8c
megane575 <rie-fttm0129@gmail.com>
2025-02-08 09:51:55 +0900

    #8 Toppageで記事一覧を表示

--- app/techhub/views.py
+++ app/techhub/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Article
 
-def hello(request):
-    return HttpResponse("Hello E-team!!")
+class TopPageView(ListView):
+    model = Article
+    template_name = "toppage.html"  # 表示用のテンプレート
+    context_object_name = "articles"
+    paginate_by = 10  # 1ページあたりの記事表示数10
+
+    def get_queryset(self):
+        return Article.objects.order_by('-posted_at')  # 記事を投稿順に取得
+
