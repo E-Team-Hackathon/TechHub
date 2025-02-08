2b547229ce72db8e37304f29d0f767a9045a8a4b
megane575 <rie-fttm0129@gmail.com>
2025-02-08 12:50:40 +0900

    コメントの追加

--- app/techhub/views.py
+++ app/techhub/views.py
@@ -11,9 +11,11 @@ class TopPageView(ListView):
         return Article.objects.order_by('-posted_at')  # 記事を投稿順に取得
     
     def get_context_data(self, **kwargs): #**kwargsは複数オブジェクトの辞書型データ
+        #ListView の デフォルトのコンテキストデータを取得してsuper（）でさらに新しいオブジェクトを追加するための変数
         context = super().get_context_data(**kwargs)
         #userとcontributorテーブルをuserカラムをもとに結合。usernameだけを取得
         unique_users = Contributor.objects.select_related('user').values('user__username').distinct()
         #辞書型リストのunique_usersをリストに変換
         context['contributors'] = [user['user__username'] for user in unique_users]
+        print('contributors')
         return context
