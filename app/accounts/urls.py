24ec1a8dbce0873cf3b6025bc0a5ec6b188e787e
megane575 <rie-fttm0129@gmail.com>
2025-02-09 14:05:06 +0900

    signup.htmlのパスの修正

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('signup/', views.SignUpView.as_view(template_name='accounts/signup.html'), name='signup'),
     path(
         'login/',
         auth_views.LoginView.as_view(redirect_authenticated_user=True,template_name='accounts/login.html'),
