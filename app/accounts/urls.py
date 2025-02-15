9b8a42973a20ee5f846e27a1a929e3f3ab5d0377
Taroimo <vcorskype@gmail.com>
2025-02-16 00:08:18 +0900

    mysiteのurls.py変更に伴う修正

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -16,6 +16,3 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # MEDIA
