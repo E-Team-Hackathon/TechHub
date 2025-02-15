3aa34701464a8be7ace45ccd42c02a79ac5f8176
Taroimo <vcorskype@gmail.com>
2025-02-15 22:34:32 +0900

    コンフリクト解消用のコミット

--- app/techhub/admin.py
+++ app/techhub/admin.py
@@ -3,8 +3,4 @@ from .models import Feed, Article, Contributor
 
 admin.site.register(Feed)
 admin.site.register(Article)
-<<<<<<< HEAD
 admin.site.register(Contributor)
-=======
-admin.site.register(Contributor)
->>>>>>> develop
