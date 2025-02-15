49efc99c9683a5b64bf8132fd4557660a4bc22d3
megane575 <rie-fttm0129@gmail.com>
2025-02-15 20:36:06 +0900

    #17　contributorテーブルでfeedとaccount_nameの重複保存を回避する

--- app/techhub/models.py
+++ app/techhub/models.py
@@ -12,6 +12,13 @@ class Contributor(models.Model):
     feed = models.ForeignKey(Feed,on_delete=models.CASCADE)
     account_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['feed', 'account_name'], name='unique_feed_account')
+        ]
+    def __str__(self):
+        return f"{self.feed.feed_name} - {self.account_name}"
     
 class Article(models.Model):
     contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
