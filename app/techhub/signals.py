e4cfb96d07d7165857cb5b63363f89097aca2ea7
megane575 <rie-fttm0129@gmail.com>
2025-02-16 17:17:23 +0900

    ＃23 投稿者登録されたらfetch＿articlesが発動されるようsignalの設定

---
+++ app/techhub/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.management import call_command
+from techhub.models import Contributor
+
+@receiver(post_save, sender=Contributor)
+def fetch_articles_after_contributor_save(sender, instance, created, **kwargs):
+  # 投稿者が登録されたらfetch＿articlesが発動される
+
+  if created :
+    print(f"Fetching articles for: {instance.account_name}")
+
+    call_command("fetch_articles")
+
+    print(f"fetch_articles executed for {instance.account_name}")
