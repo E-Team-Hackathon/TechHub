67ccdd0d574ec06bc753e82b54eda34e53d3c142
megane575 <rie-fttm0129@gmail.com>
2025-02-23 12:22:05 +0900

    #31　軽微な修正

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -23,9 +23,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
+            raise ValueError(('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+            raise ValueError(('Superuser must have is_superuser=True.'))
         
         return self.create_user(username, password, **extra_fields)
 
